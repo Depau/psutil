74be17f2f59ca82773ee6111244f4c09893923cf
Davide Depau <me@davideddu.org>
2018-04-13 18:00:38 +0200

    Remove python_requires

--- setup.py
+++ setup.py
@@ -326,7 +326,6 @@ def main():
     )
     if setuptools is not None:
         kwargs.update(
-            python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
             test_suite="psutil.tests.get_suite",
             tests_require=[
                 'ipaddress; python_version < "3.3"',
